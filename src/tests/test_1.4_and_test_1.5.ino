252a61f18596f20e7e19e39ff8df0291615ff58e
Rizelle <32849880+rvinl001@users.noreply.github.com>
2021-07-17 00:09:48 -0700

    Add files via upload

---
+++ src/tests/test_1.4_and_test_1.5.ino
@@ -0,0 +1,72 @@
+/* Name: touch_motion_sensor
+ * Description: This file is used solely to test the functionality motion and touch sensors.
+ * Date: 7/16/2021
+
+ * The circuit:
+ * LCD RS pin to digital pin 8
+ * LCD Enable pin to digital pin 9
+ * LCD D4 pin to digital pin 10
+ * LCD D5 pin to digital pin 11
+ * LCD D6 pin to digital pin 12
+ * LCD D7 pin to digital pin 13
+ * Touch sensor to digital pin 7
+ * Motion sensor to digital pin 6
+
+*/
+
+// include the library code for LCD Screen:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
+const int TouchPin = 7;
+const int MotionPin = 6;
+
+void setup() {
+  // define inputs and outputs
+  pinMode(TouchPin, INPUT);
+  pinMode(MotionPin, INPUT);
+  lcd.begin(16, 2);
+
+}
+
+void loop() {
+  
+  int sensorDetected = digitalRead(TouchPin);
+  int motionDetected = digitalRead(MotionPin);
+  
+  // sensor is being touched, motion sensor detects movement
+  if(sensorDetected == 1 && motionDetected == 1)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("ur touching me ;)");
+    lcd.setCursor(0, 1);
+    lcd.print("i see you");
+  }
+  
+  // sensor is being touched, motion sensor doesnt detect movement
+  else if (sensorDetected == 1 && motionDetected == 0)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("ur touching me ;)");    
+    lcd.setCursor(0, 1);
+    lcd.print("i dont see you");
+  }
+  // sensor isnt being touched, motion sensor detects movement
+  else if (sensorDetected == 0 && motionDetected == 1)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("ur not touching me :(");    
+    lcd.setCursor(0, 1);
+    lcd.print("i see you");
+  }
+  // sensor isnt being touched, motion sensor doesnt detects movement
+  else
+  {
+    lcd.setCursor(0, 0);
+    lcd.print("ur not touching me :(");
+    lcd.setCursor(0, 1);
+    lcd.print("i dont see you");
+  }
+}
